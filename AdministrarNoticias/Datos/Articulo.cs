60499ad4b20a048f43c9ab3f9069e0afd65e11a3
CAMIL EDU DOROTEO MEDINA <cd2018-0268@uce.edu.do>
2021-07-15 20:30:45 -0400

    Required

--- AdministrarNoticias/Datos/Articulo.cs
+++ AdministrarNoticias/Datos/Articulo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -7,13 +8,18 @@ namespace AdministrarNoticias
 {
     public partial class Articulo
     {
+       
         public int ArticuloId { get; set; }
+        [Required(ErrorMessage = "Campo Obligatorio")]
         public string Titulo { get; set; }
         public string Descripcion { get; set; }
         public string Autor { get; set; }
+        [Required(ErrorMessage = "Campo Obligatorio")]
         public string Url { get; set; }
         public string UrlToImage { get; set; }
+
         public DateTime? FechaPublicacion { get; set; }
+        [Required(ErrorMessage = "Campo Obligatorio")]
         public string Contenido { get; set; }
         public int? CategoriaId { get; set; }
         public int? PaisId { get; set; }
